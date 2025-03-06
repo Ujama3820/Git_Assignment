37ca02b8aeb7572525503b72786869bb7a2fe483
Ujama3820 <shaikhujama7@gmail.com>
2025-03-06 11:07:03 +0530

    adding index file to git

---
+++ index.js
@@ -0,0 +1,13 @@
+const express= require('express');
+
+const server= express();
+
+server.get('/',(req,res)=>{
+    res.send("Welcome to My Application")
+})
+
+server.get('/users',(req,res)=>{
+    res.send([{id:1,name:"Uzma"},{id:2,name:"Uzma"},{id:3,name:"Uzma"}])
+})
+
+server.listen(3000,()=>console.log("Server Started"))

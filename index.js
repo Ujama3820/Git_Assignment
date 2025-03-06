63988b8a757fd08f34978a4a36dc02cd7e05833c
Ujama3820 <shaikhujama7@gmail.com>
2025-03-06 13:24:00 +0530

    modified

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ server.get('/',(req,res)=>{
     res.send("Welcome to My Application")
 })
 
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT || 3000;
 
 server.get('/users',(req,res)=>{
     res.send([{id:1,name:"Uzma"},{id:2,name:"Uzma"},{id:3,name:"Uzma"}])
 })
 
-server.listen(PORT,'0.0.0.0',()=>{console.log("Server Started")})
+server.listen(PORT,'0.0.0.0',()=> {console.log("Server Started")})

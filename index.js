8da8035d742caea4dc95d4494b24a4a1057c2b9d
Ujama3820 <shaikhujama7@gmail.com>
2025-03-06 15:18:42 +0530

    added

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ server.get('/users',(req,res)=>{
     res.send([{id:1,name:"Uzma"},{id:2,name:"Uzma"},{id:3,name:"Uzma"}])
 })
 
-server.listen(5000,'0.0.0.0',() => {console.log("Server Started")})
+server.listen(PORT,'0.0.0.0', () => {console.log("Server Started")})

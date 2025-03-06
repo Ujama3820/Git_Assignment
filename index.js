20376f9117ec1af67dcec2513f195988210fdea7
Ujama3820 <shaikhujama7@gmail.com>
2025-03-06 13:46:06 +0530

    modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express= require('express');
 
 const server= express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.get('/',(req,res)=>{
     res.send("Welcome to My Application")

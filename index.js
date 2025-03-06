adf30aa0c342f3c4030ccf2747d4dbc3a5df53eb
Ujama3820 <shaikhujama7@gmail.com>
2025-03-06 13:13:51 +0530

    added port

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ server.get('/',(req,res)=>{
     res.send("Welcome to My Application")
 })
 
+const PORT = process.env.PORT ||3000;
+
 server.get('/users',(req,res)=>{
     res.send([{id:1,name:"Uzma"},{id:2,name:"Uzma"},{id:3,name:"Uzma"}])
 })
 
-server.listen(3000,'0.0.0.0',()=>{console.log("Server Started")})
+server.listen(PORT,'0.0.0.0',()=>{console.log("Server Started")})
